3c2205c9b229c39a4bb0bff538ed40fdc05cc9d3
tom <1157164295@qq.com>
2020-04-30 11:31:50 +0800

    修改:历史趋势图tooltip显示信息

--- src/app/routes/data-query/data-query.component.ts
+++ src/app/routes/data-query/data-query.component.ts
@@ -3,25 +3,20 @@ import { STChange, STPage, STColumn, STComponent } from '@delon/abc';
 import { RequireService } from '@core/require';
 import { _HttpClient } from '@delon/theme';
 import { ApiService } from '@core/api.service';
-import {
-  SFSchema,
-  FormProperty,
-  PropertyGroup,
-  SFDateWidgetSchema,
-  SFRadioWidgetSchema,
-  SFComponent,
-} from '@delon/form';
+import { SFSchema, SFComponent } from '@delon/form';
 import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 // import { of } from 'rxjs';
 // import { delay } from 'rxjs/operators';
 // declare var G2: any;
+let DESC = [];
+let UNIT = [];
 @Component({
   selector: 'app-data-query',
   templateUrl: './data-query.component.html',
   styles: [],
 })
 export class DataQueryComponent implements OnInit {
-  constructor(private require: RequireService, public http: _HttpClient, private api: ApiService) { }
+  constructor(private require: RequireService, public http: _HttpClient, private api: ApiService) {}
   getMySelection = this.require.api.getMySelection;
   deleteUrl = this.require.api.selectionDelete;
   data = []; // 保存表格信息
@@ -71,7 +66,7 @@ export class DataQueryComponent implements OnInit {
                     break;
                 }
               },
-              err => { },
+              err => {},
             );
           },
         },
@@ -272,16 +267,15 @@ export class DataQueryComponent implements OnInit {
             this.option.series = objArr;
             // console.log(this.option)
             this.option = { ...this.option };
-            console.log(this.option);
+            // console.log(this.option);
           } else {
             // this.gatewayNumber = '';
             this.require.message.info('数据为空', { nzDuration: 1000 });
             this.option.legend.data = [];
             this.option.xAxis.data = [];
             this.option.series = [];
-            console.log(this.option)
+            // console.log(this.option)
             this.option = { ...this.option };
-
           }
           break;
         default:
@@ -292,19 +286,42 @@ export class DataQueryComponent implements OnInit {
   // 趋势图初始化
   chart() {
     this.option = {
-      color: ['#17c0eb', '#2ecc71', '#f19066', '#e74c3c', '#3498db', '#9b59b6', '#2c3e50', '#c44569', '#3dc1d3', '#f8a5c2'],
+      // color: ['#66b7f2', '#79e095', '#fad183', '#fc9e90', '#7edad3', '#999ab4', '#7a9ae5', '#f1a867', '#f1e289', '#9f8cd6'],
+      // color: ['#fa2c7b', '#ff38e0', '#ffa235', '#04c5f3', '#0066fe', '#8932a5', '#c90444', '#cb9bff', '#434348', '#90ed7d', '#f7a35c', '#8085e9'],
+      // color: ['#00a8e1', '#99cc00', '#e30039', '#fcd300', '#800080', '#00994e', '#ff6600', '#808000', '#db00c2', '#008080', '#0000ff', '#c8cc00'],
+      color: [
+        '#0e72cc',
+        '#6ca30f',
+        '#f59311',
+        '#fa4343',
+        '#16afcc',
+        '#85c021',
+        '#d12a6a',
+        '#0e72cc',
+        '#6ca30f',
+        '#f59311',
+        '#fa4343',
+        '#16afcc',
+      ],
       title: {
         text: '历史数据曲线',
       },
       tooltip: {
         trigger: 'axis',
-        formatter: function (params) {
-          var result = '';
-          params.forEach(function (item) {
-            result += item.marker + " " + item.seriesName + " : " + item.value + "</br>";
+        formatter: params => {
+          let result = '';
+          let i = 0;
+          result += `${params[0].name}<br/>`;
+          params.forEach(item => {
+            result += `${item.marker} ${item.seriesName} : ${
+              typeof item.value === 'undefined' ? '数据为空' : DESC[i]
+            }&nbsp;&nbsp;${typeof item.value === 'undefined' ? ' ' : item.value}&nbsp;${
+              typeof item.value === 'undefined' ? ' ' : UNIT[i]
+            }<br/>`; //  ${this.desc[i]}
+            i++;
           });
           return result;
-        }
+        },
       },
       legend: {
         data: [],
@@ -326,19 +343,19 @@ export class DataQueryComponent implements OnInit {
         data: [],
         axisLine: {
           lineStyle: {
-            color: '#546de5',
+            color: '#cfcfcf',
             width: 2,
-          }
-        }
+          },
+        },
       },
       yAxis: {
         type: 'value',
         axisLine: {
           lineStyle: {
-            color: '#546de5',
+            color: '#cfcfcf',
             width: 2,
-          }
-        }
+          },
+        },
       },
       dataZoom: [
         {
@@ -449,6 +466,8 @@ export class DataQueryComponent implements OnInit {
       this.ps = ret.ps;
     } else if (ret.type === 'checkbox') {
       this.labelArr = ret.checkbox.map(e => e.dtuSection + e.dtuAddress);
+      DESC = ret.checkbox.map(e => e.propertyDesc);
+      UNIT = ret.checkbox.map(e => e.propertyUnit);
       this.checked = ret.checkbox.map(e => e.id);
     }
   }
@@ -474,7 +493,7 @@ export class DataQueryComponent implements OnInit {
                   break;
               }
             },
-            err => { },
+            err => {},
           );
         },
       });
@@ -522,7 +541,7 @@ export class DataQueryComponent implements OnInit {
             break;
         }
       },
-      err => { },
+      err => {},
     );
   }
 
